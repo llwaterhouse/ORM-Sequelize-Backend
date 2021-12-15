8a9857490403cec14c34a8b1529cc96e513e732b
Linda LW <lindalw03@gmail.com>
2021-12-15 17:48:16 -0500

    Fixed category DELETE error by adding onDelete:CASCADE to product.belongsTo as well.

--- models/index.js
+++ models/index.js
@@ -8,6 +8,7 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
+  onDelete: "CASCADE"
 });
 
 // Categories have many Products
@@ -21,7 +22,8 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through:ProductTag,
-  foreignKey: "product_id"
+  foreignKey: "product_id",
+ 
 });
 
 // Tags belongToMany Products (through ProductTag)
